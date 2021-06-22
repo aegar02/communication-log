3ab19f971e6a05a88b57c85e7b16f4d86211a117
Guillermo Jose Frenero-Sanchez <guigui@MacBook-Pro.local>
2021-06-22 12:49:35 -0400

    Changed background color.

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -6,7 +6,8 @@ namespace communication_log
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello pilot!");
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Hello Major Tom!");
             Console.WriteLine("Hello Mission Control!");
             Console.WriteLine("Copy and received, Pilot!");
             Console.WriteLine("This is Major Tom to ground control, I'm stepping through the door.");
