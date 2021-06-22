d4488cf2f8a60724d09649a9835a37a4db4b115f
Aaron <aaron@MacBook-Air.local>
2021-06-22 12:05:29 -0500

    Added a fifth line.

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -10,6 +10,7 @@ namespace communication_log
             Console.WriteLine("Hello Mission Control!");
             Console.WriteLine("Copy and received, Pilot!");
             Console.WriteLine("This is Major Tom to ground control, I'm stepping through the door.");
+            Console.WriteLine("Ground Control to Major Tom, Godspeed!");
         }
     }
 }
