1184572705a69c3ecbf7db8572908dd5095c111c
Aaron <aaron@MacBook-Air.local>
2021-06-22 14:03:20 -0500

    Back to original.

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -10,7 +10,6 @@ namespace communication_log
             Console.WriteLine("Hello Mission Control!");
             Console.WriteLine("Copy and received, Pilot!");
             Console.WriteLine("This is Major Tom to ground control, I'm stepping through the door.");
-            Console.WriteLine("Ground Control to Major Tom, Godspeed!");
         }
     }
 }
